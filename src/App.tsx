141e1ee87d45fca1c21da5d6707f58ed837eadcd
chae <yce0222@gmail.com>
2025-01-04 02:25:30 +0900

    feat: router 정리

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './styles/global.css'
 import Home from "./pages/Home.tsx";
 import Checklist from "./pages/Checklist.tsx";
-import StockDetails from "./pages/Stock_Details.tsx";
-import GuideDetail from "./pages/GuideDetail.tsx";
+import StockDetails from "./pages/StockDetails.tsx";
 import DefaultLayout from "./components/DefaultLayout.tsx";
 
 const App = () => {
@@ -13,8 +12,7 @@ const App = () => {
           <Routes>
             <Route path={"/"} element={<Home />} />
             <Route path={"/checklist"} element={<Checklist />} />
-            <Route path={"/guide/:id"} element={<GuideDetail />} />
-            <Route path="/details/:id" Component={StockDetails} />
+            <Route path={"/stockdetail/:id"} element={<StockDetails />} />
           </Routes>
         </DefaultLayout>
     </BrowserRouter>
