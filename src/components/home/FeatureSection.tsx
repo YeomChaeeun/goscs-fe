d4316d415b16d0f96d06bd8bf0156e31a0490e30
chae <yce0222@gmail.com>
2025-01-05 00:08:49 +0900

    fix: home style

--- src/components/home/FeatureSection.tsx
+++ src/components/home/FeatureSection.tsx
@@ -1,77 +1,72 @@
-import { styled } from "@mui/material/styles";
-import { Box, Container, Typography } from "@mui/material";
-import { Component } from "react";
+import {styled} from "@mui/material/styles";
+import {Box, Container, Typography} from "@mui/material";
+import {recommendations} from "../../data/recommendations.ts";
+import {InvestmentProfile} from "../../data/questions.ts";
+import {useSetRecoilState} from "recoil";
+import {assetState, checklistState} from "../../recoil/atoms/assetAtom.ts";
+import {useNavigate} from "react-router-dom";
 
 const FeaturesSection = styled(Box)(({ theme }) => ({
-  padding: theme.spacing(8, 4),
-  backgroundColor: theme.palette.background.paper,
-  textAlign: "center",
-  color: "white",
+    minHeight: '80vh',
+    padding: theme.spacing(12, 4),
+    backgroundImage: 'linear-gradient(45deg, #FF6B6B, #c53b2f)',
+    textAlign: 'center',
+    color: 'white',
 }));
 
 const FeatureCard = styled(Box)(({ theme }) => ({
-  padding: theme.spacing(3),
-  backgroundColor: "white",
-  borderRadius: theme.spacing(2),
-  margin: theme.spacing(2),
-  height: "200px",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  color: "black",
+    padding: theme.spacing(3),
+    backgroundColor: 'white',
+    borderRadius: theme.spacing(2),
+    margin: theme.spacing(2),
+    height: '200px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'black',
+    cursor: 'pointer'
 }));
 
-class FeatureSection extends Component {
-  render() {
+const FeatureSection = () => {
+    const navigate = useNavigate()
+    const setState = useSetRecoilState(checklistState);
+    const setAssetStateRecoil = useSetRecoilState(assetState);
+
+    const handleSelectProfile = (profile: InvestmentProfile) => {
+        setState((prev) => ({ ...prev, profile: profile }));
+        setAssetStateRecoil(profile);
+        navigate("/checklist/result");
+    };
+
     return (
       <FeaturesSection>
-        <Typography variant="h4" gutterBottom>
-          There are three investment tendencies. Try it 😘✨.
-        </Typography>
-        <Container maxWidth="md" sx={{ mt: 4 }}>
-          <Box
-            sx={{
-              display: "grid",
-              gridTemplateColumns: {
-                xs: "1fr",
-                sm: "1fr 1fr",
-                md: "1fr 1fr 1fr",
-              },
-              gap: 3,
-            }}
-          >
-            <FeatureCard>
-              <Typography variant="h6" gutterBottom>
-                IPO Access
-              </Typography>
-              <Typography>
-                Be the first in line for companies going public at the IPO
-                price.
-              </Typography>
-            </FeatureCard>
-            <FeatureCard>
-              <Typography variant="h6" gutterBottom>
-                Recurring Investment
-              </Typography>
-              <Typography>
-                Add to your wealth on a consistent basis, even when you forget
-                to.
-              </Typography>
-            </FeatureCard>
-            <FeatureCard>
-              <Typography variant="h6" gutterBottom>
-                Fractional Shares
-              </Typography>
-              <Typography>
-                Own shares in the companies you love for as little as $1.
-              </Typography>
-            </FeatureCard>
-          </Box>
-        </Container>
+          <Typography variant="h3" gutterBottom>
+              There are three investment tendencies. Try it 😘✨.
+          </Typography>
+          <Container maxWidth="md" sx={{mt: 4}}>
+              <Box sx={{
+                  display: "grid",
+                  gridTemplateColumns: {
+                      xs: "1fr",
+                      sm: "1fr 1fr",
+                      md: "1fr 1fr 1fr"
+                  },
+                  gap: 3
+              }}>
+                  {(Object.keys(recommendations) as InvestmentProfile[]).map(profile => {
+                      return (
+                        <FeatureCard onClick={()=> { handleSelectProfile(profile) }}>
+                            <Typography variant="h6" gutterBottom>
+                                {profile}
+                            </Typography>
+                        </FeatureCard>
+                      );
+                  })}
+              </Box>
+          </Container>
       </FeaturesSection>
     );
-  }
-}
+};
 
 export default FeatureSection;
